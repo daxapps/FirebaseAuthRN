e748ffbf932b2398340a3bce092d245817a92ae0
Jason Crawford <daxapps777@gmail.com>
2019-06-04 14:41:21 -0400

    demo complete

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import * as firebase from "firebase";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+//import all screens
+import HomeScreen from "./screens/HomeScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import SignupScreen from "./screens/SignupScreen";
+import SignInScreen from "./screens/SignInScreen";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBKZ9drJjZlm0bHkLaDz-IUCkNM--wnnqY",
+  authDomain: "reactbootcamp-b3005.firebaseapp.com",
+  databaseURL: "https://reactbootcamp-b3005.firebaseio.com",
+  projectId: "reactbootcamp-b3005",
+  storageBucket: "reactbootcamp-b3005.appspot.com",
+  messagingSenderId: "542600800622",
+  appId: "1:542600800622:web:8377d8f0f09a6126"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const MainNavigator = createStackNavigator(
+  {
+    Loading: { screen: LoadingScreen },
+    SignIn: { screen: SignInScreen },
+    SignUp: { screen: SignupScreen },
+    Home: { screen: HomeScreen }
   },
-});
+  {
+    //launcher screen
+    initialRouteName: "Loading"
+  }
+);
+
+//create app container
+const App = createAppContainer(MainNavigator);
+export default App;
